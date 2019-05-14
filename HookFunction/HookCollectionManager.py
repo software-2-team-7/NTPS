629939d7430a399de33adc853e55e0a08e49edd7
SpottieTea <octobersushi@yahoo.com>
2019-05-13 22:56:47 -0600

    add Bianca's sorting function.

--- HookFunction/HookCollectionManager.py
+++ HookFunction/HookCollectionManager.py
@@ -12,6 +12,17 @@ class HookCollectionManager(object):
             newPacket = h.executeHookSequence(newPacket)
         return newPacket
 
+    def insertionSort_Hook_Collection_Sequence_Number(self):
+        for index in range(1,len(self.collection)):
+            currentHookCollection = self.collection[index]
+            currentvalue = self.collection[index].Hook_Collection_Sequence_Number
+            position = index
+            while position>0 and self.collection[position-1].Hook_Collection_Sequence_Number > currentvalue:
+                self.collection[position]=self.collection[position-1]
+                position = position-1
+
+            self.collection[position]=currentHookCollection
+
     def deleteCollection(self):
         print("Collection deleted.")
     
